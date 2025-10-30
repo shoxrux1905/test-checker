e087cdefc622bbf2470a9796df9ed4155752530a
Ilhombek <rahmonovilhombek9@gmail.com>
2025-10-30 19:48:16 +0500

    Submission api

--- apps/assignments/urls.py
+++ apps/assignments/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-
+from apps.assignments.api_endpoints.submission.views import SubmissionListCreateAPIView
 
 urlpatterns = [
-    
+    path('submissions/', SubmissionListCreateAPIView.as_view(), name='submission-list-create'),
 ]
