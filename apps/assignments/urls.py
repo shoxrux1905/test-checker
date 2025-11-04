fc2a419340ab4433dd5e6875a868aa71db57a681
Your NameShohjahon2707 <saidovshohjahon000@gmail.com>
2025-11-04 19:45:07 +0500

    assignments api

--- apps/assignments/urls.py
+++ apps/assignments/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 from apps.assignments.api_endpoints.assignments.views import AssignmentListCreateAPIView, AssignmentDetailAPIView
-from apps.assignments.api_endpoints.submissions_serializers.views import SubmissionListCreateAPIView, SubmissionDetailAPIView
+
 
 urlpatterns = [
     path('assignments/', AssignmentListCreateAPIView.as_view(), name='assignment-list-create'),
     path('assignments/<int:pk>/', AssignmentDetailAPIView.as_view(), name='assignment-detail'),
-    path('submissions/', SubmissionListCreateAPIView.as_view(), name='submission-list-create'),
-    path('submissions/<int:pk>/', SubmissionDetailAPIView.as_view(), name='submission-detail'),
+
 ]
