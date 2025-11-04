0822361f2896f9cf9b7c5d3d1802f8aa423b606d
Ilhombek <rahmonovilhombek9@gmail.com>
2025-11-04 19:15:40 +0500

    create detail

---
+++ apps/assignments/api_endpoints/submission/views/submission_create_view.py
@@ -0,0 +1,23 @@
+
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status,permissions
+from apps.assignments.api_endpoints.submission.serializers import SubmissionSerializer
+
+
+class SubmissionCreateAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+
+    def post(self,request):
+        data = request.data.copy()
+        data["student"] = request.user.id
+
+        serializer = SubmissionSerializer(data=data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+
--- apps/assignments/urls.py
+++ apps/assignments/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from apps.assignments.api_endpoints.submission.views import SubmissionListCreateAPIView
+from apps.assignments.api_endpoints.submission.views.submission_create_view import SubmissionCreateAPIView
+from apps.assignments.api_endpoints.submission.views.submission_detail_view import SubmissionDetailAPIView
 
 urlpatterns = [
-    path('submissions/', SubmissionListCreateAPIView.as_view(), name='submission-list-create'),
+    path('submissions/create/', SubmissionCreateAPIView.as_view(), name='submission-create'),
+    path('submissions/<int:pk>/', SubmissionDetailAPIView.as_view(), name='submission-detail'),
 ]
